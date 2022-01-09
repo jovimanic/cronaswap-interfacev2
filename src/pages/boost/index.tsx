4e932d7e0b3cc1736dc66bf8eeb5926c3bb812de
BladeCKG <neverdreamagain9106@gmail.com>
2022-01-09 23:22:18 +0800

    feat(pages): changed address codes

--- src/pages/boost/index.tsx
+++ src/pages/boost/index.tsx
@@ -187,15 +187,187 @@ export default function Stake() {
         />
       </Head>
       <div className="flex flex-col w-full min-h-full">
-        <div className="flex justify-center mb-6">
+        <div className="grid grid-cols-12 gap-4 min-h-1/2">
+          <div className="hidden lg:block lg:col-span-4 h-full" style={{ maxHeight: '40rem' }}>
+            <div className="flex flex-col space-y-4">
+              <div className="bg-blue bg-opacity-40 p-8 rounded-lg">
+                <div className="flex flex-row items-center justify-between">
+                  <h1 className="text-2xl">veCRONA APY</h1>
+                  <div className="flex flex-col items-end">
+                    <h1 className="text-xl">119.68%</h1>
+                    <h2 className="text-sm opacity-50">Average last week</h2>
+                  </div>
+                </div>
+              </div>
+
+              <div className="p-8 pb-64 rounded-lg bg-dark-900">
+                <h1 className="text-lg mb-4">What’s CRONA Boost?</h1>
+                <p className="text-sm mb-8 text-dm-text-secondary">
+                  CRONA Boost is your opportunity to increase the power and rewards of your CRONA. The longer you lock
+                  your CRONA the greater your voting power (CRONA Powah) and weekly reward share will be. You can boost
+                  up to 4x by locking CRONA for 4 years. Your boosts slowly reduces over your locking period, eventually
+                  unlocking your CRONA, however you can always increase your boost to maintain your boosted level. Your
+                  boosted CRONA is represented in eCRONA (escrowed CRONA) and will remain in escrow until your unlock
+                  date (non-transferable).
+                </p>
+
+                <div className="hidden: md:block flex flex-col space-y-2">
+                  <div className="p-4 rounded-lg bg-dark-800">
+                    <h1 className="text-lg">50.88%</h1>
+                    <h2 className="text-sm flex flex-row items-center">
+                      % of CRONA locked <span style={{ marginLeft: 4 }}>x</span>
+                    </h2>
+                  </div>
+                  <div className="p-4 rounded-lg bg-dark-800">
+                    <h1 className="text-lg">2.54 years</h1>
+                    <h2 className="text-sm flex flex-row items-center">
+                      Average CRONA lock time <span style={{ marginLeft: 4 }}>x</span>
+                    </h2>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-span-12 lg:col-span-8 h-full" style={{ minHeight: '40rem' }}>
+            <div className="lg:hidden mb-4 space-y-4">
+              <div className="bg-light-yellow bg-opacity-40 p-8 rounded-lg">
+                <div className="flex flex-row items-center justify-between">
+                  <h1 className="text-2xl">veCRONA APY</h1>
+                  <div className="flex flex-col items-end">
+                    <h1 className="text-xl">119.68%</h1>
+                    <h2 className="text-sm opacity-50">Average last week</h2>
+                  </div>
+                </div>
+              </div>
+              <div className="p-8 pb-64 rounded-lg bg-dark-900">
+                <h1 className="text-lg mb-4">What’s CRONA Boost?</h1>
+                <p className="text-sm mb-8 text-dm-text-secondary">
+                  CRONA Boost is your opportunity to increase the power and rewards of your CRONA. The longer you lock
+                  your CRONA the greater your voting power (CRONA Powah) and weekly reward share will be. You can boost
+                  up to 4x by locking CRONA for 4 years. Your boosts slowly reduces over your locking period, eventually
+                  unlocking your CRONA, however you can always increase your boost to maintain your boosted level. Your
+                  boosted CRONA is represented in eCRONA (escrowed CRONA) and will remain in escrow until your unlock
+                  date (non-transferable).
+                </p>
+              </div>
+            </div>
+
+            <div className="flex flex-col space-y-4">
+              <div className="p-8 rounded-lg bg-dark-900">
+                <div className="mb-4">
+                  <div className="flex flex-row item-center justify-between text-lg">
+                    <span>Rewarded last week</span>
+                    <span>156,626.096 CRONA</span>
+                  </div>
+                  <div className="flex flex-row item-center justify-between text-lg">
+                    <span>Last week</span>
+                    <span>$117,469.572</span>
+                  </div>
+                </div>
+                <p className="text-sm text-dm-text-tertiary">
+                  Weekly Qi staking rewards include 30% of the repayment fees for all collateral types plus a 25% weekly
+                  boost (converted into Qi). Rewards also include 100% of the farming rewards from the deposit fee
+                  revenue used to farm Qi-MATIC.
+                  <br />
+                  <br />
+                  Revenues are collected weekly and distributed on the following Wednesday.
+                </p>
+              </div>
+
+              {/* lock crona */}
+              <div className="rounded-lg bg-dark-900">
+                <div className="p-8 rounded-t-lg bg-dark-800">
+                  <h1 className="text-lg">CRONA Boost Lock</h1>
+                </div>
+                <div className="p-8 space-y-4">
+                  <div className="flex items-center justify-between w-full">
+                    <p className="font-bold text-large md:text-2xl text-high-emphesis">{i18n._(t`Lock CRONA`)}</p>
+                    <div className="text-high-emphesis text-xs font-medium md:text-base md:font-normal">
+                      Balance: 1,230.0 CRONA
+                    </div>
+                  </div>
+
+                  <Input.Numeric
+                    value={input}
+                    onUserInput={handleInput}
+                    className={classNames(
+                      'w-full h-14 px-3 md:px-5 mt-5 rounded bg-dark-800 text-sm md:text-lg font-bold text-dark-800 whitespace-nowrap caret-high-emphesis',
+                      inputError ? ' pl-9 md:pl-12' : ''
+                    )}
+                    placeholder=" "
+                  />
+
+                  {/* input overlay: */}
+                  <div className="relative w-full h-0 pointer-events-none bottom-20">
+                    <div
+                      className={`flex justify-between items-center h-14 rounded px-3 md:px-5 ${
+                        inputError ? ' border border-red' : ''
+                      }`}
+                    >
+                      <div className="flex space-x-2 ">
+                        {inputError && (
+                          <Image
+                            className="mr-2 max-w-4 md:max-w-5"
+                            src="/error-triangle.svg"
+                            alt="error"
+                            width="20px"
+                            height="20px"
+                          />
+                        )}
+                        <p
+                          className={`text-sm md:text-lg font-bold whitespace-nowrap ${
+                            input ? 'text-high-emphesis' : 'text-secondary'
+                          }`}
+                        >
+                          {`${input ? input : '0'} ${activeTab === 0 ? '' : 'x'}SUSHI`}
+                        </p>
+                      </div>
+                      <div className="flex items-center text-sm text-secondary md:text-base">
+                        <button
+                          className="px-2 py-1 ml-3 text-xs font-bold border pointer-events-auto focus:outline-none focus:ring hover:bg-opacity-40 md:bg-cyan-blue md:bg-opacity-30 border-secondary md:border-cyan-blue rounded-2xl md:py-1 md:px-3 md:ml-4 md:text-sm md:font-normal md:text-cyan-blue"
+                          onClick={handleClickMax}
+                        >
+                          {i18n._(t`MAX`)}
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="grid grid-cols-3 gap-4 md:grid-cols-6">
+                    <button className="rounded-lg p-3 bg-dark-700">1 Week</button>
+                    <button className="rounded-lg p-3 bg-dark-700">1 Month</button>
+                    <button className="rounded-lg p-3 bg-dark-700">3 Months</button>
+                    <button className="rounded-lg p-3 bg-dark-700">6 Months</button>
+                    <button className="rounded-lg p-3 bg-dark-700">1 Year</button>
+                    <button className="rounded-lg p-3 bg-dark-700">3 Years</button>
+                  </div>
+
+                  <div className="flex flex-col space-y-2 pb-4 px-4">
+                    <div className="flex flex-row justify-between text-lg">
+                      <h1>My CRONA Locked</h1> <span>1223.998</span>
+                    </div>
+                    <div className="flex flex-row justify-between text-lg">
+                      <h1>My veCRONA balance</h1> <span>1223.998</span>
+                    </div>
+                    <div className="flex flex-row justify-between text-lg">
+                      <h1>Unlock time</h1> <span>2021-11-23 10:34:23</span>
+                    </div>
+                    <div className="flex flex-row justify-between text-lg">
+                      <h1>Boost multiplier</h1> <span>1.00x</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* <div className="flex justify-center mb-6">
           <div className="flex flex-col w-full max-w-xl mt-auto mb-2">
             <div className="flex max-w-lg">
               <div className="self-end mb-3 text-lg font-bold md:text-2xl text-high-emphesis md:mb-7">
                 {i18n._(t`Maximize yield by staking SUSHI for xSUSHI`)}
               </div>
-              {/* <div className="self-start pl-6 pr-3 mb-1 min-w-max md:hidden">
-                                <img src={XSushiSignSmall} alt="xsushi sign" />
-                            </div> */}
             </div>
             <div className="max-w-lg pr-3 mb-2 text-sm leading-5 text-gray-500 md:text-base md:mb-4 md:pr-0">
               {i18n._(t`For every swap on the exchange on every chain, 0.05% of the swap fees are distributed as SUSHI
@@ -204,25 +376,63 @@ export default function Stake() {
                                 Your xSUSHI is continuously compounding, when you unstake you will receive all the originally deposited
                                 SUSHI and any additional from fees.`)}
             </div>
-            {/* <div className="flex">
-                            <div className="mr-14 md:mr-9">
-                                <StyledLink className="text-sm text-lg whitespace-nowrap md:text-lg md:leading-5">
-                                    Enter the Kitchen
-                                </StyledLink>
-                            </div>
-                            <div>
-                                <StyledLink className="text-sm text-lg whitespace-nowrap md:text-lg md:leading-5">
-                                    Tips for using xSUSHI
-                                </StyledLink>
-                            </div>
-                        </div> */}
           </div>
           <div className="hidden px-8 ml-6 md:block w-72">
             <Image src="/xsushi-sign.png" alt="xSUSHI sign" width="100%" height="100%" layout="responsive" />
           </div>
-        </div>
+        </div> */}
         <div className="flex flex-col justify-center md:flex-row">
-          <div className="flex flex-col w-full max-w-xl mx-auto mb-4 md:m-0">
+          <div className="w-full max-w-xl mx-auto md:mx-0 md:block md:w-72">
+            <div className="flex flex-col w-full px-4 pt-6 pb-5 rounded bg-dark-900 md:px-8 md:pt-7 md:pb-9">
+              <div className="flex flex-wrap">
+                <div className="flex flex-col flex-grow md:mb-14">
+                  <p className="mb-3 text-lg font-bold md:text-2xl md:font-medium text-high-emphesis">
+                    {i18n._(t`Balance`)}
+                  </p>
+                  <div className="flex items-center space-x-4">
+                    <Image
+                      className="max-w-10 md:max-w-16 -ml-1 mr-1 md:mr-2 -mb-1.5 rounded"
+                      src="/images/tokens/xsushi-square.jpg"
+                      alt="xSUSHI"
+                      width={64}
+                      height={64}
+                    />
+                    <div className="flex flex-col justify-center">
+                      <p className="text-sm font-bold md:text-lg text-high-emphesis">
+                        {xSushiBalance ? xSushiBalance.toSignificant(4) : '-'}
+                      </p>
+                      <p className="text-sm md:text-base text-primary">xSUSHI</p>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="flex flex-col flex-grow">
+                  <div className="flex mb-3 ml-8 flex-nowrap md:ml-0">
+                    <p className="text-lg font-bold md:text-2xl md:font-medium text-high-emphesis">
+                      {i18n._(t`Unstaked`)}
+                    </p>
+                    {/* <img className="w-4 ml-2 cursor-pointer" src={MoreInfoSymbol} alt={'more info'} /> */}
+                  </div>
+                  <div className="flex items-center ml-8 space-x-4 md:ml-0">
+                    <Image
+                      className="max-w-10 md:max-w-16 -ml-1 mr-1 md:mr-2 -mb-1.5 rounded"
+                      src="/images/tokens/sushi-square.jpg"
+                      alt="SUSHI"
+                      width={64}
+                      height={64}
+                    />
+                    <div className="flex flex-col justify-center">
+                      <p className="text-sm font-bold md:text-lg text-high-emphesis">
+                        {sushiBalance ? sushiBalance.toSignificant(4) : '-'}
+                      </p>
+                      <p className="text-sm md:text-base text-primary">SUSHI</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col w-full max-w-xl mx-auto mb-4 md:m-0 md:ml-6 ">
             <div className="mb-4">
               <div className="flex items-center justify-between w-full h-24 max-w-xl p-4 rounded md:pl-5 md:pr-7 bg-light-yellow bg-opacity-40">
                 <div className="flex flex-col">
@@ -230,7 +440,6 @@ export default function Stake() {
                     <p className="text-sm font-bold whitespace-nowrap md:text-lg md:leading-5 text-high-emphesis">
                       {i18n._(t`Staking APR`)}{' '}
                     </p>
-                    {/* <img className="ml-3 cursor-pointer" src={MoreInfoSymbol} alt={'more info'} /> */}
                   </div>
                   <div className="flex">
                     <a
@@ -379,84 +588,6 @@ export default function Stake() {
                       : i18n._(t`Confirm Withdrawal`)}
                   </button>
                 )}
-              </div>
-            </div>
-          </div>
-          <div className="w-full max-w-xl mx-auto md:mx-0 md:ml-6 md:block md:w-72">
-            <div className="flex flex-col w-full px-4 pt-6 pb-5 rounded bg-dark-900 md:px-8 md:pt-7 md:pb-9">
-              <div className="flex flex-wrap">
-                <div className="flex flex-col flex-grow md:mb-14">
-                  <p className="mb-3 text-lg font-bold md:text-2xl md:font-medium text-high-emphesis">
-                    {i18n._(t`Balance`)}
-                  </p>
-                  <div className="flex items-center space-x-4">
-                    <Image
-                      className="max-w-10 md:max-w-16 -ml-1 mr-1 md:mr-2 -mb-1.5 rounded"
-                      src="/images/tokens/xsushi-square.jpg"
-                      alt="xSUSHI"
-                      width={64}
-                      height={64}
-                    />
-                    <div className="flex flex-col justify-center">
-                      <p className="text-sm font-bold md:text-lg text-high-emphesis">
-                        {xSushiBalance ? xSushiBalance.toSignificant(4) : '-'}
-                      </p>
-                      <p className="text-sm md:text-base text-primary">xSUSHI</p>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="flex flex-col flex-grow">
-                  <div className="flex mb-3 ml-8 flex-nowrap md:ml-0">
-                    <p className="text-lg font-bold md:text-2xl md:font-medium text-high-emphesis">
-                      {i18n._(t`Unstaked`)}
-                    </p>
-                    {/* <img className="w-4 ml-2 cursor-pointer" src={MoreInfoSymbol} alt={'more info'} /> */}
-                  </div>
-                  <div className="flex items-center ml-8 space-x-4 md:ml-0">
-                    <Image
-                      className="max-w-10 md:max-w-16 -ml-1 mr-1 md:mr-2 -mb-1.5 rounded"
-                      src="/images/tokens/sushi-square.jpg"
-                      alt="SUSHI"
-                      width={64}
-                      height={64}
-                    />
-                    <div className="flex flex-col justify-center">
-                      <p className="text-sm font-bold md:text-lg text-high-emphesis">
-                        {sushiBalance ? sushiBalance.toSignificant(4) : '-'}
-                      </p>
-                      <p className="text-sm md:text-base text-primary">SUSHI</p>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="flex flex-col w-full mb-4 mt-7 md:mb-0">
-                  {/* <div className="flex items-center justify-between">
-                        <div className="flex items-center flex-1 flex-nowrap">
-                            <p className="text-base font-bold md:text-lg text-high-emphesis">Weighted APR</p>
-                            <img className="w-4 ml-2 cursor-pointer" src={MoreInfoSymbol} alt={'more info'} />
-                        </div>
-                        <div className="flex flex-1 md:flex-initial">
-                            <p className="ml-5 text-base text-primary md:ml-0">{`${weightedApr}%`}</p>
-                        </div>
-                    </div> */}
-                  {account && (
-                    <a
-                      href={`https://analytics.sushi.com/users/${account}`}
-                      target="_blank"
-                      rel="noreferrer noopener"
-                      className={`
-                                flex flex-grow justify-center items-center
-                                h-14 mt-6 rounded
-                                bg-dark-700 text-high-emphesis
-                                focus:outline-none focus:ring hover:bg-opacity-80
-                                text-sm font-bold cursor-pointer
-                            `}
-                    >
-                      {i18n._(t`Your SushiBar Stats`)}
-                    </a>
-                  )}
-                </div>
               </div>
             </div>
           </div>
