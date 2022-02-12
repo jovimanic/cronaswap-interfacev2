6456a144815072cb07cc058ac9ba0be4bf113cab
BladeCKG <neverdreamagain9106@gmail.com>
2022-02-12 22:04:09 +0800

    feat: add menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -266,6 +266,18 @@ function AppBar(): JSX.Element {
                   </Link>
                 )}
 
+                {chainId && featureEnabled(Feature.IFO, chainId) && (
+                  <Link href={'/ifov2'}>
+                    <a
+                      id={`farm-nav-link`}
+                      className="p-2 text-baseline text-primary hover:text-high-emphesis focus:text-high-emphesis md:p-3 whitespace-nowrap"
+                    >
+                      {' '}
+                      {i18n._(t`IFO`)}
+                    </a>
+                  </Link>
+                )}
+
                 {chainId && featureEnabled(Feature.STAKING, chainId) && (
                   <Link href={'/stake'}>
                     <a
