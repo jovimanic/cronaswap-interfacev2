f03fca76039448ccb8ad7cbf2eabc43c7232db4b
BladeCKG <neverdreamagain9106@gmail.com>
2022-03-23 14:31:29 +0800

    feat: update ifo

--- src/constants/ifo.ts
+++ src/constants/ifo.ts
@@ -9,28 +9,28 @@ const ifos: Ifo[] = [
   {
     id: 'mifo',
     address: {
-      [ChainId.CRONOS]: '0xC28a8dc376A64eBc37E189F66CDB12a4CBDC7595',
+      [ChainId.CRONOS]: '0x2c741797403b1F486EE28116091Bf86293C90b53',
       [ChainId.BSC_TESTNET]: '0xD1e0Da69F2Ee9B7d3602DB9F41F37beE2d99F176',
     },
     isActive: true,
     name: 'ARCANE',
     poolBasic: {
-      saleAmount: '1,200,000',
-      raiseAmount: '$180,000',
+      saleAmount: '1,350,000',
+      raiseAmount: '$202,500',
       cronaToBurn: '$90,000',
       distributionRatio: 0.3,
       raiseToken: CRONA,
     },
     poolUnlimited: {
-      saleAmount: '3,800,000',
-      raiseAmount: '$420,000',
+      saleAmount: '2,650,000',
+      raiseAmount: '$397,500',
       cronaToBurn: '$110,000',
       distributionRatio: 0.7,
       raiseToken: USDC,
     },
     // raiseToken: USDC,
     offerToken: {
-      [ChainId.CRONOS]: new Token(ChainId.CRONOS, '0x78f0953f2d07eB8B50397415e85b97F3360211c7', 18, 'ARCANE', 'ARCANE'),
+      [ChainId.CRONOS]: new Token(ChainId.CRONOS, '0x289B8f552c9fD66f9cEA35B193F7ca73ae24A5d5', 18, 'ARC', 'ARCANE'),
       [ChainId.BSC_TESTNET]: new Token(
         ChainId.BSC_TESTNET,
         '0xf2C856AB8Ed6f67Fd7D45Fd017c811347bF01a28',
