c70413cbd95dd1bf0fc6d312296f68879858e996
BladeCKG <neverdreamagain9106@gmail.com>
2022-03-28 11:36:07 +0700

    1. Add GameFi DiceRoll CoinToss Page 2. Implement Landing Page from figma design

--- src/pages/gamefi/index.tsx
+++ src/pages/gamefi/index.tsx
@@ -2,70 +2,101 @@ import React, { useState } from 'react'
 import { useLingui } from '@lingui/react'
 import Head from 'next/head'
 import Container from '../../components/Container'
+import { t } from '@lingui/macro'
+import Image from 'app/components/Image'
+import NavLink from 'app/components/NavLink'
 
-const people = [
-  {
-    name: 'Coin Toss',
-    role: 'Heads or Tails, with odds of winning at 50:50. Payouts are up to 1.98x!',
-    imageUrl: 'https://croissant.games/icons/coin-flip/coin.png',
-    gameFiUrl: 'https://croissant.games/coin-toss/CRONA',
-  },
-  {
-    name: 'CRO Roll',
-    role: 'You are winning if the number lands between your selected range. Win up to 19x your wager',
-    imageUrl: 'https://croissant.games/icons/cro-roll.png',
-    gameFiUrl: 'https://croissant.games/cro-roll/CRONA',
-  },
+const games = [
   {
     name: 'Dice Roll',
-    role: 'Guess the dice face that comes up. Payouts are up to 5.94x !',
-    imageUrl: 'https://croissant.games/icons/dice/dice.png',
-    gameFiUrl: 'https://croissant.games/dice-roll/CRONA',
+    role: 'Guess the dice face that comes up. Payouts are up to 5.94x',
+    logoImageUrl: `url('/images/pages/landing/diceroll/logo.png')`,
+    backImageUrl: `url('/images/pages/landing/diceroll/back.png')`,
+    gameFiUrl: '/diceroll',
+    gameFiTitle: 'Dice Roll',
+    description: 'Guess the dice face that comes up. Payouts are up to 5.94x',
   },
   {
-    name: 'Roulette',
-    role: 'Choose numbers from 0 to 36. Payouts are up to 34.65x !',
-    imageUrl: 'https://croissant.games/icons/roulette/roulette.png',
-    gameFiUrl: 'https://croissant.games/roulette/CRONA',
+    name: 'Coin Toss',
+    role: 'Choose numbers from from 0 to 36. Payouts are up to 34.65X',
+    logoImageUrl: `url('/images/pages/landing/cointoss/logo.png')`,
+    backImageUrl: `url('/images/pages/landing/cointoss/back.png')`,
+    gameFiUrl: '/cointoss',
+    gameFiTitle: 'Coin Toss',
+    description: 'Choose numbers from from 0 to 36. Payouts are up to 34.65X',
+  },
+  {
+    name: 'Lottery(Comming Soon)',
+    role: 'CRONA lottery is coming soon. We will let you know immediately it’s out.',
+    logoImageUrl: `url('/images/pages/landing/lottery/logo.png')`,
+    backImageUrl: `url('/images/pages/landing/lottery/back.png')`,
+    gameFiUrl: '/lottery',
+    gameFiTitle: 'Lottery',
+    description: 'CRONA lottery is coming soon. We will let you know immediately it’s out.',
   },
 ]
 
 export default function GameFi() {
   const { i18n } = useLingui()
   return (
-    <Container id="gamefi-page" className="py-4 md:py-8 lg:py-12" maxWidth="full">
+    <Container id="gamefi-page" maxWidth="full" className="py-4 md:py-8 lg:py-24">
       <Head>
         <title key="title">GameFi | CronaSwap</title>
-        <meta key="description" name="description" content="Boost CronaSwap" />
+        <meta key="description" name="description" content="Welcome to CronaSwap" />
       </Head>
-      <div className="mx-auto px-4 max-w-7xl sm:px-6 lg:px-8">
-        <div className="space-y-12">
-          <div className="space-y-5 sm:space-y-4 md:max-w-xl lg:max-w-3xl xl:max-w-none">
-            <h2 className="text-3xl font-extrabold text-white tracking-tight sm:text-4xl">CROissant Games</h2>
-            <p className="text-xl text-gray-300">
-              GameFi partner - user playing any of the games will not require any gas fees from the player end!
-            </p>
+      <div className="relative flex flex-col items-center w-full">
+        <div className="absolute top-1/4 -left-10 bg-blue bottom-4 w-3/5 rounded-full z-0 filter blur-[150px]" />
+        <div className="absolute bottom-1/4 -right-10 bg-red top-4 w-3/5 rounded-full z-0  filter blur-[150px]" />
+        {/* <img src="images/pages/landing/center-ellipse.png" className="" /> */}
+        <div className="relative filter drop-shadow">
+          <div className="mt-[100px] flex flex-col items-center">
+            <div className="text-[5vw] font-bold text-white font-sans leading-[89.3px]">Welcome to Crona Game</div>
+            <div className="max-w-[469px] text-center text-white text-[18px] leading-[24px] mt-[14px]">
+              GameFi partner - user playing any of the games will not require any gas fees from the player end
+            </div>
+            <NavLink
+              href={{
+                pathname: 'diceroll',
+              }}
+            >
+              <button className="mt-[40px] text-white bg-blue rounded px-[48px] py-[18px] hover:bg-light-blue">
+                Play Now
+              </button>
+            </NavLink>
           </div>
-          <ul role="list" className="space-y-4 sm:grid sm:grid-cols-2 sm:gap-6 sm:space-y-0 lg:grid-cols-2 lg:gap-8">
-            {people.map((person) => (
-              <li key={person.name} className="py-10 px-6 bg-gray-800 text-center rounded-lg xl:px-10 xl:text-left">
-                <div className="space-y-6 xl:space-y-10">
-                  <img className="mx-auto h-20 w-20 xl:w-56 xl:h-56" src={person.imageUrl} alt="" />
-                  <div className="space-y-2 xl:flex xl:items-center xl:justify-between">
-                    <a
-                      href={person.gameFiUrl}
-                      target="_blank"
-                      rel="noreferrer"
-                      className="font-medium text-lg leading-6 space-y-1"
+          <div className="px-32 pt-[100px]">
+            <ul role="list" className="grid sm:grid-cols-1 sm:gap-6 lg:grid-cols-3 lg:gap-10">
+              {games.map((game) => (
+                <li key={game.name}>
+                  <NavLink
+                    href={{
+                      pathname: game.gameFiUrl,
+                    }}
+                  >
+                    <button
+                      className="w-[368px] h-[373px] hover:border rounded-2xl"
+                      style={{
+                        backgroundImage: game.backImageUrl,
+                      }}
                     >
-                      <h3 className="text-white">{person.name}</h3>
-                      <p className="text-indigo-400">{person.role}</p>
-                    </a>
-                  </div>
-                </div>
-              </li>
-            ))}
-          </ul>
+                      <div className="px-[37px] pt-[54px] text-white text-center h-full bg-[#0D0C2B80] rounded-2xl">
+                        <div
+                          className="w-[184px] h-[187px] mx-[54px]"
+                          style={{
+                            backgroundImage: game.logoImageUrl,
+                            backgroundRepeat: 'no-repeat',
+                            backgroundPosition: 'center',
+                          }}
+                        ></div>
+                        <p className="font-bold text-[24px] leading-[29.77px]">{game.gameFiTitle}</p>
+                        <p className="font-[500]">{game.description}</p>
+                      </div>
+                    </button>
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     </Container>
